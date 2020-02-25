a89c18df78ce198a5ebce120250dcc56534e4779
Sebastian <s163526@student.dtu.dk>
2020-02-25 19:10:02 +0100

    Working

--- src/SMTPConnection.java
+++ src/SMTPConnection.java
@@ -1,105 +1,97 @@
-import com.sun.xml.internal.messaging.saaj.soap.Envelope;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.rmi.server.ExportException;
-import java.util.StringTokenizer;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 /**
- * Open an SMTP connection to a mailserver and send one mail.
+ * Open an SMTP connection to a remote machine and send one mail.
  */
 public class SMTPConnection {
+    /* The socket to the server */
+    public Socket connection;
+
+    /* Streams for reading and writing the socket */
+    public BufferedReader fromServer;
+    public DataOutputStream toServer;
+
+    /* Just to make it look nicer */
     private static final int SMTP_PORT = 25;
     private static final String CRLF = "\r\n";
-    /* The socket to the server */
-    private Socket connection;
-    /* Streams for reading and writing the socket */
-    private BufferedReader fromServer;
-    private DataOutputStream toServer;
+
     /* Are we connected? Used in close() to determine what to do. */
     private boolean isConnected = false;
 
-    /* Create an SMTPConnection object. Create the socket and the 
-       associated streams. Initialize SMTP connection. */
+    /* Create an SMTPConnection object. Create the socket and the
+       associated streams. Send HELO-command and check for errors. */
     public SMTPConnection(Envelope envelope) throws IOException {
-        //  connection = new Socket("hostname", 8686);
-        fromServer = new BufferedReader(new InputStreamReader(System.in)); //connection.getInputStream()
-        toServer = new DataOutputStream(System.out);
+        connection = new Socket(envelope.DestAddr, SMTP_PORT);
+        fromServer = new
+                BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer = new DataOutputStream(connection.getOutputStream());
 
-        /* Fill in */
-	/* Read a line from server and check that the reply code is 220.
-	   If not, throw an IOException. */
-        int i= parseReply(fromServer.readLine());
-        if(i!=220) {}
-        else throw new IOException();
-        /* Fill in */
-
-	/* SMTP handshake. We need the name of the local machine.
-	   Send the appropriate SMTP handshake command. */
-        String localhost = "10.16.168.255";
-        //sendCommand(   );
-
+        String reply = fromServer.readLine();
+        if(parseReply(reply) != 220) {
+            System.out.println("Error in connect.");
+            System.out.println(reply);
+            return;
+        }
+        String localhost = (InetAddress.getLocalHost()).getHostName();
+        try {
+            sendCommand("HELO " + localhost, 250);
+        } catch (IOException e) {
+            System.out.println("HELO failed. Aborting.");
+            return;
+        }
         isConnected = true;
     }
 
-    /* Send the message. Write the correct SMTP-commands in the
-       correct order. No checking for errors, just throw them to the
-       caller. */
+    /* Send the message. Simply writes the correct SMTP-commands in the
+       correct order. No checking for errors, just throw them to
+       the caller. */
     public void send(Envelope envelope) throws IOException {
-        /* Fill in */
-	/* Send all the necessary commands to send a message. Call
-	   sendCommand() to do the dirty work. Do _not_ catch the
-	   exception thrown from sendCommand(). */
-        /* Fill in */
+        sendCommand("MAIL FROM: <" + envelope.Sender + ">", 250);
+        sendCommand("RCPT TO: <" + envelope.Recipient + ">", 250);
+        sendCommand("DATA", 354);
+        sendCommand(envelope.Message.toString() + CRLF + ".", 250);
     }
 
-    /* Close the connection. First, terminate on SMTP level, then
-       close the socket. */
+    /* Close the connection. Try to send QUIT-commmand and then close
+       the socket. */
     public void close() {
         isConnected = false;
         try {
-            sendCommand( /* Fill in */);
-            // connection.close();
+            sendCommand("QUIT", 221);
+            connection.close();
         } catch (IOException e) {
             System.out.println("Unable to close connection: " + e);
             isConnected = true;
         }
     }
 
-    /* Send an SMTP command to the server. Check that the reply code is
-       what is is supposed to be according to RFC 821. */
+    /* Send an SMTP command to the server. Check for reply code. Does
+       not check for multiple reply codes (required for RCPT TO). */
     private void sendCommand(String command, int rc) throws IOException {
+        String reply = null;
 
-        /* Fill in */
-        /* Write command to server and read reply from server. */
-       toServer.writeBytes(command);
-       int i= parseReply(fromServer.readLine());
-        /* Fill in */
+        toServer.writeBytes(command + CRLF);
+        reply = fromServer.readLine();
+        if(parseReply(reply) != rc) {
+            System.out.println("Error in command: " + command);
+            System.out.println(reply);
+            throw new IOException();
+        }
 
-        /* Fill in */
-	/* Check that the server's reply code is the same as the parameter
-	   rc. If not, throw an IOException. */
-	    if(i!=rc){
-
-        } else throw new IOException();
-
-        /* Fill in */
     }
 
     /* Parse the reply line from the server. Returns the reply code. */
     private int parseReply(String reply) {
-        StringTokenizer tokenizer = new StringTokenizer(reply);
-        String token = tokenizer.nextToken();
-        return new Integer(token);
-
+        StringTokenizer parser = new StringTokenizer(reply);
+        String replycode = parser.nextToken();
+        return (new Integer(replycode)).intValue();
     }
 
     /* Destructor. Closes the connection if something bad happens. */
     protected void finalize() throws Throwable {
-        if (isConnected) {
+        if(isConnected) {
             close();
         }
         super.finalize();
